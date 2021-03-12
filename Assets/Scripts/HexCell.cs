ab085225a58d260a95f7edd61a39d3d349b54ffb
Ertuğ Obalar <eobalar@hotmail.com>
2021-03-12 15:09:49 +0100

    Added Hex Grids and scripts for visualization

---
+++ Assets/Scripts/HexCell.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+    public HexCoordinates coordinates;
+    public LineRenderer lineRenderer;
+
+
+    public void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+    void Start()
+    {
+        DrawHexagon(this);
+    }
+
+    Vector3[] HexagonVertices(HexCell cell)
+    {
+        Vector3 center = cell.transform.localPosition;
+        Vector3[] hexagonVectices = new Vector3[HexMetrics.corners.Length];
+        for (int i = 0; i < HexMetrics.corners.Length; i++)
+        {
+            hexagonVectices[i] = center + HexMetrics.corners[i];
+        }
+        return hexagonVectices;
+    }
+    void DrawHexagon(HexCell cell)
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = HexMetrics.corners.Length;
+        lineRenderer.SetPositions(HexagonVertices(cell));
+
+    }
+}
