4dfcb605a01991b074b56fa0ce1abbe3c9595e6f
Kaan Kotan <kaankotan@Kaans-MacBook-Pro.local>
2021-03-28 23:37:55 +0200

    Add Text Input for Integers

---
+++ Assets/Scripts/UI/NumberInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberInput : MonoBehaviour
+{
+    public GameObject inputField;
+    public CameraMovement cameraComponent;
+
+	void Update()
+	{
+        inputField = GameObject.Find("/-UI/Canvas/UI_TextInput/NumberInput");
+        cameraComponent = Camera.main.GetComponent<CameraMovement>();
+		Debug.Log(inputField);
+        Debug.Log(inputField.GetComponent<InputField>());
+        Debug.Log(inputField.GetComponent<InputField>().isFocused);
+        if(inputField.GetComponent<InputField>().isFocused == true) {
+            cameraComponent.setMovable(false);
+        } else {
+            cameraComponent.setMovable(true);
+        }
+	}
+}
---
+++ Assets/Scripts/UI/UI_TextInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_TextInput : MonoBehaviour
+{
+
+    // public void Awake() {
+    //     Hide();
+    // }
+
+    public void Show() {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    public void GetInput() {
+        GameObject inputField = GameObject.Find("/Canvas/UI_TextInput/NumberInput/Text");
+        InputField inputFieldCo = inputField.GetComponent<InputField>();
+        Debug.Log(inputFieldCo.text);
+    }
+
+    
+}
