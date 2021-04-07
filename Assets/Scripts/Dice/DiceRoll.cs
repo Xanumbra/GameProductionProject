0cb4dc1bc15dae7737987e1b4c713ba18fe575a9
Ertuğ Obalar <eobalar@hotmail.com>
2021-04-07 13:23:18 +0200

    Added DiceRoll class

---
+++ Assets/Scripts/Dice/DiceRoll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoll : MonoBehaviour
+{
+    public static int diceOne;
+    public static int diceTwo;
+    public static int RollDice()
+    {
+        diceOne = Random.Range(1, 7);
+        diceTwo = Random.Range(1, 7);
+        return diceOne + diceTwo;
+    }
+}
