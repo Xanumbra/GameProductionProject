d17677e863576a202a9b7846f52a5e696f4d3fa9
Ertuğ Obalar <eobalar@hotmail.com>
2021-04-25 12:55:57 +0200

    Added InfoBox

---
+++ Assets/Scripts/UI/InfoBoxManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoBoxManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static InfoBoxManager _instance;
+
+    public static Text infoBox;
+    public Text infoBoxHolder;
+    public static InfoBoxManager Instance { get { return _instance; } }
+    void Awake()
+    {
+       if( _instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        infoBox = infoBoxHolder;
+    }
+    private void Start()
+    {
+        writeMessage("Hello, world!");
+        writeMessage("Test Message");
+    }
+
+    static void writeMessage(string message)
+    {
+        infoBox.text += "\n" + message;
+    }
+    static void playerTurnMessage(string playername)
+    {
+        infoBox.text += "\nIt's "+ playername +"'s turn.";
+    }
+}
