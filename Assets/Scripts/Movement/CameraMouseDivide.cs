0d3f890005c74080deae13586d75c0e70ffecf12
KAAN KOTAN <kaankotan93@gmail.com>
2021-05-03 23:01:48 +0200

    UI V1

---
+++ Assets/Scripts/Movement/CameraMouseDivide.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseDivide : MonoBehaviour
+{
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+    private int someInt = 0;
+    bool clicked = false;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform cameraInit = Camera.main.transform;
+        var screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        screenPoint.z = 10.0f; //distance of the plane from the camera
+        Debug.Log(screenPoint);
+        //Debug.Log(Camera.main.ScreenToWorldPoint(screenPoint));
+        if (Input.GetMouseButtonDown(0))
+        {
+            clicked = true;
+            return;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //Debug.Log("Up");
+            clicked = false;
+        }
+        if(clicked == true) {
+            if(Input.mousePosition.x > Screen.width/2) {
+                Vector3 sth = new Vector3(0, 1, 0);
+                transform.RotateAround(target.transform.position, sth, 60 * Time.deltaTime);
+            } else {
+                Vector3 sth = new Vector3(0, 1, 0);
+                transform.RotateAround(target.transform.position, sth, -60 * Time.deltaTime);
+            }
+        }
+        //Debug.Log(Camera.main.transform.position);
+    }
+}
---
+++ Assets/Scripts/Movement/RotateAround.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    //Assign a GameObject in the Inspector to rotate around
+    public GameObject target;
+
+    void Update()
+    {
+        // Spin the object around the target at 20 degrees/second.
+        Vector3 sth = new Vector3(500000, 500000, 50000);
+        transform.RotateAround(target.transform.position, sth, 20 * Time.deltaTime);
+    }
+}
