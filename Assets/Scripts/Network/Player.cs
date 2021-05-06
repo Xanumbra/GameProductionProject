c34b946cb62febd53e2bd673281caadfb8bfbcba
Ertuğ Obalar <eobalar@hotmail.com>
2021-05-06 12:52:22 +0200

    Update Player.cs
    Trying to fix writer bug in Mirror.

--- Assets/Scripts/Network/Player.cs
+++ Assets/Scripts/Network/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using System.Linq;
-
+[System.Serializable]
 public class Player : NetworkBehaviour
 {
     public static Player localPlayer;
