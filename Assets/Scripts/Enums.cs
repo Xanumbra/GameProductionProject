53f5302b449cb682257ec74610312611e67c4233
Sichmann <exploder@gmx.at>
2021-06-18 19:36:01 +0200

    Added Trails to Spaceships

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -7,7 +7,7 @@ public class Enums
         darkMatter, //#3
         spacePig,   //#4
         water,      //#4
-        metal,      //#3
+        metal,      //#3 
         energy,     //#4
         sun         //#1
     }
