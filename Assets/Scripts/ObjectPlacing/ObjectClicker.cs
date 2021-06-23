b54e4346d5396cd337ec3a03c87fd01a3843eb9a
Ertuğ Obalar <eobalar@hotmail.com>
2021-06-23 03:14:20 +0200

    One Line to Build them All.
    Spent fucking 3 hours to just comment a line and solve the bug.

--- Assets/Scripts/ObjectPlacing/ObjectClicker.cs
+++ Assets/Scripts/ObjectPlacing/ObjectClicker.cs
@@ -10,7 +10,7 @@ public class ObjectClicker : MonoBehaviour
 
     private void Awake()
     {
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
     }
 
     private void OnEnable()
